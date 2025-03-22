6a629d30905e344dbb7ecc92050b5de95ac3f223
Doaa Gamal <doaa.gamal02@gmail.com>
2025-03-22 17:07:30 +0200

    trying e2e on ec2

--- cypress/e2e/e2e-test.cy.js
+++ cypress/e2e/e2e-test.cy.js
@@ -1,6 +1,6 @@
 describe('E2E Test for Angular App', () => {
     it('Visits the main page and checks for content', () => {
-      cy.visit('http://localhost:4200');
+      cy.visit('http://13.60.99.107:32320/');
       cy.contains('Trending Products');
     });
   });
